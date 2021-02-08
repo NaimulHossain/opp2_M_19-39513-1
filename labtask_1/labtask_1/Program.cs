0733028d27afb6fae7b0842da3f26838960589ae
Your Name <naimulrohan789456123@gmail.com>
2021-02-08 10:42:52 +0600

    lab3

---
+++ labtask_1/labtask_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace lab_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 0;
+
+
+
+            Console.WriteLine("Even Numbers :");
+            for (i = 1; i <= 50; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + "\n");
+                }
+            }
+        }
+    }
+}
