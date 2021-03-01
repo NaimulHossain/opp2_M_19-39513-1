6e88463faa2d5bcfcde420675bd35d99c5efcb6a
Your Name <naimulrohan789456123@gmail.com>
2021-03-01 11:05:22 +0600

    Labtask6-3/1/2021

---
+++ labtask6/labtask6/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labtask6
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {​​​​​​​
+      
+            Student stu1 = new Student("19-39537-1", "SHOUHARDO", 3.31F);
+            Student stu2 = new Student("19-39537-2", "NAIMUL", 3.81F);
+            Student stu3 = new Student("19-39537-3", "AK AZAD", 3.5F);
+
+
+            Department dept1 = new Department("CS", "Computer Science");
+            Department dept2 = new Department("EE","Electronic Engineering");
+
+
+            stu1.Dept = dept1;
+            stu2.Dept = dept2;
+            stu3.Dept = dept2;
+
+
+            stu1.ShowInfo();
+            stu1.ShowDepartment();
+
+
+            stu2.ShowInfo();
+            stu2.ShowDepartment();
+
+
+            stu3.ShowInfo();
+            stu3.ShowDepartment();
+
+
+          
+            dept1.Name = "Computer Science and Engineering";
+
+
+            stu1.ShowInfo();
+            stu1.ShowDepartment();
+
+
+
+            dept1.AddStudent(stu1);
+            dept1.AddStudent(stu2);
+
+
+            dept1.PrintStudent();
+        }​​​​​​​
+    }​​​​​​​
+}​​​​​​​
+
+
+
+
+
+
+
+
+
+
+
+
+
